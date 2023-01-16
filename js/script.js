03714a5cdf993aa20adeea399790ca38bcfde63e
Gabriel Campos <gabrielfcampos2017@gmail.com>
2023-01-16 17:15:01 -0300

    Better carousel :p

--- js/script.js
+++ js/script.js
@@ -1,33 +1,77 @@
 $(document).ready(function(){
-   /*
     let slideAtual = 1
-    let listaSlides = ["banner-f1", "banner-cs", "banner-lol"]
+    let listaSlides = ["url(../../img/valorantBanner.png)", "url(../../img/csBanner.png)", "url(../../img/sonaBanner.png)"]
 
-    setInterval(mudarSlide, 5000)
-    function mudarSlide()
-        if(slideAtual > 0){
-            $("#carrossel").removeClass(listaSlides[slideAtual - 1])
-        }else{
-            $("#carrossel").removeClass(listaSlides[listaSlides.length - 1])
-        }      
+    setInterval(mudarSlideDireita, 5000)
+/*resetar timer se botão clicado*/
+    function mudarSlideEsquerda()  {
         
+         if(slideAtual == 2){
+            $("#bannerImg").css("background-image",listaSlides[1]);
+            slideAtual = 1;
 
-        //exibir slide atual
-        $("#carrossel").addClass(listaSlides[slideAtual])
+            $("#radioBanner0").prop('checked', false);
+            $("#radioBanner1").prop('checked', true);
+            $("#radioBanner2").prop('checked', false);
 
-        //indicar qual slide atual
-        console.log("Slide atual é", slideAtual)
-        slideAtual ++
+        }else if(slideAtual == 1){
+            $("#bannerImg").css("background-image",listaSlides[0])
+            slideAtual = 0;
 
-        if(slideAtual > listaSlides.length - 1){
-            slideAtual = 0
-        }
+            $("#radioBanner0").prop('checked', true);
+            $("#radioBanner1").prop('checked', false);
+            $("#radioBanner2").prop('checked', false);
 
-    }*/
+        }else{
+            $("#bannerImg").css("background-image",listaSlides[2])
+            slideAtual = 2;
 
+            $("#radioBanner0").prop('checked', false);
+            $("#radioBanner1").prop('checked', false);
+            $("#radioBanner2").prop('checked', true);
 
+        }    
 
+    }
 
+    function mudarSlideDireita()  {
+        
+        if(slideAtual == 2){
+           $("#bannerImg").css("background-image",listaSlides[0]);
+           slideAtual = 0;
+
+           $("#radioBanner0").prop('checked', true);
+           $("#radioBanner1").prop('checked', false);
+           $("#radioBanner2").prop('checked', false);
+
+       }else if(slideAtual == 1){
+           $("#bannerImg").css("background-image",listaSlides[2])
+           slideAtual = 2;
+
+           $("#radioBanner0").prop('checked', false);
+           $("#radioBanner1").prop('checked', false);
+           $("#radioBanner2").prop('checked', true);
+
+       }else{
+           $("#bannerImg").css("background-image",listaSlides[1])
+           slideAtual = 1;
+
+           $("#radioBanner0").prop('checked', false);
+           $("#radioBanner1").prop('checked', true);
+           $("#radioBanner2").prop('checked', false);
+
+       }    
+
+   }
+
+    
+    $("#bannerLeftArrow").click(function(){
+        mudarSlideEsquerda()
+    })
+
+    $("#bannerRightArrow").click(function(){
+        mudarSlideDireita()
+    })
 
 
 
